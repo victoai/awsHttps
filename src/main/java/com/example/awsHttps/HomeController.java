567701781cbb8e9e3176e09ac3615fc553a662d1
victoai <victoai@naver.com>
2025-01-02 21:52:28 +0900

    homecontroller 수정

--- src/main/java/com/example/awsHttps/HomeController.java
+++ src/main/java/com/example/awsHttps/HomeController.java
@@ -4,8 +4,9 @@ package com.example.awsHttps;
 import jdk.jfr.Category;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
 
